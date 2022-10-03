4aa71e00fc506febbb28272cdfe5c5c33da90eb9
Anys82 <107157073+Anys82@users.noreply.github.com>
2022-10-03 18:48:34 -0300

    Create Mock-Tasks.ts

---
+++ Mock-Tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from "./Task"
+export const TASK: Task[] = [
+    {
+        id: 1,
+        Text: "terminar primer modulo",
+        day: "Octubre 3",
+        remander: true
+    },
+    {
+        id: 2,
+        Text:"Hacer compras",
+        day: "Octubre 5",
+        remander: true
+    },
+    {
+        id: 3,
+        Text:"Comida Chornita",
+        day: "Octubre 5",
+        remander: false
+    }
+]
