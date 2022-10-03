940c1e8d9f132a8df8842396d4e991b76ee74d0c
Anys82 <107157073+Anys82@users.noreply.github.com>
2022-10-03 18:51:00 -0300

    Update button.components.ts

--- button.components.ts
+++ button.components.ts
@@ -1,1 +1,16 @@
-<button class="btn">{{text}}</button>
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+@Input() text: string = ""; 
+@Input() color: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
